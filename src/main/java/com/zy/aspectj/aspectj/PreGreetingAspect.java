8cfdd35a0c96915820a96ee0295e823db5862549
zy <weyrue@gmail.com>
2019-11-29 17:24:17 +0800

    test aop

--- src/main/java/com/zy/aspectj/aspectj/PreGreetingAspect.java
+++ src/main/java/com/zy/aspectj/aspectj/PreGreetingAspect.java
@@ -1,13 +1,23 @@
 package com.zy.aspectj.aspectj;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-import org.springframework.stereotype.Component;
 
 @Aspect
 public class PreGreetingAspect {
     @Before("execution(* greetTo(..))")
     public void beforeGreeting() {
         System.out.println("How are you");
+    }
+
+    @Around("execution(* serveTo(..))")
+    public void aroundServing(ProceedingJoinPoint pjp) throws Throwable {
+        System.out.println(pjp.getSignature().toLongString());
+        Object[] objs = new Object[1];
+        objs[0] = "Tom";
+        pjp.proceed(objs);
+        System.out.println(pjp.getSignature().getName() + " is end.");
     }
 }
